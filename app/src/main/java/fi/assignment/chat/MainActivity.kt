5e41a4842c77c57bd625ed6a9e3d2deca606ace5
Abdeljalil Misbah <jalil.misbah91@gmail.com>
2021-05-03 18:30:40 +0300

    Part 3 understood and implemented

--- app/src/main/java/fi/assignment/chat/MainActivity.kt
+++ app/src/main/java/fi/assignment/chat/MainActivity.kt
@@ -1,14 +1,21 @@
 package fi.assignment.chat
 
+import android.app.AlertDialog
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.util.Log
 import android.view.KeyEvent
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -17,21 +24,28 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private val TAG: String = MainActivity::class.java.name
+    private lateinit var auth: FirebaseAuth
+    private var currentUser: FirebaseUser? = null
+
     private lateinit var messages: ArrayList<String>
     private lateinit var database: DatabaseReference
     private lateinit var edMessage: EditText
     private lateinit var rcMessageList: RecyclerView
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
         edMessage = findViewById(R.id.messageText)
-        rcMessageList=findViewById(R.id.messageList)
+        rcMessageList = findViewById(R.id.messageList)
 
         database = Firebase.database.reference
-        messages = arrayListOf()
+        auth = Firebase.auth
+
+        messages = arrayListOf<String>()
 
         edMessage.setOnKeyListener { v,keyCode,event ->
             if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
@@ -60,6 +74,49 @@ class MainActivity : AppCompatActivity() {
         rcMessageList.adapter = MyAdapter(messages)
     }
 
+    override fun onStart() {
+        super.onStart()
+        loginDialog()
+    }
+
+    fun loginDialog() {
+        val builder = AlertDialog.Builder( this)
+
+        with(builder){
+            val linearLayout: LinearLayout =LinearLayout(this@MainActivity)
+            linearLayout.orientation = LinearLayout.VERTICAL
+
+            val inputEmail: EditText = EditText(this@MainActivity)
+            inputEmail.inputType =
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS or InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+            inputEmail.hint= "Enter Email"
+            linearLayout.addView(inputEmail)
+
+            val inputPw: EditText= EditText(this@MainActivity)
+            inputPw.inputType=
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            inputPw.hint = "Enter Password"
+            linearLayout.addView(inputPw)
+            builder.setView(linearLayout)
+
+            builder.setPositiveButton("OK") { dialog, which ->
+                login(inputEmail.text.toString(), inputPw.text.toString())
+            }.show()
+        }
+    }
+
+    fun login(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){ task ->
+                if (task.isSuccessful){
+                    Log.d(TAG,"singInWithEmail:success")
+                    currentUser= auth.currentUser
+                }else{
+                    Log.w(TAG, "SingInWithEmail:failure", task.exception)
+                    Toast.LENGTH_SHORT.toShort()
+                }
+            }
+    }
 
 
     private fun addMessage() {
